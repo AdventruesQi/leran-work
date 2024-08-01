ca457aa9b9c1acb436732f5cecd3153bb292c319
Nintendo <3550505854@qq.com>
2024-08-01 15:41:29 +0800

    feat: commitlint的husky提交代码钩子

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Nintendo
+ * @Date: 2024-08-01 15:36:52
+ * @LastEditors: Nintendo
+ * @LastEditTime: 2024-08-01 15:37:26
+ * @Description: 
+ */
+module.exports = {
+  extends: ['./packages/commitlint-config/index.js'],
+};
